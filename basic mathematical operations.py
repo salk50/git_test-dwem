bfec467f581e8fde614f46cb6794e4d8dfcee453
Muhammad Salman Khalid <salu.kh90@gmail.com>
2021-03-14 00:52:32 +0300

    Edited code to 'basic mathematical operations.py'

---
+++ basic mathematical operations.py
@@ -0,0 +1,24 @@
+def Add(x, y):
+    z=x+y
+    print ('%d + %d = %d\n' %(x, y, z))
+
+def Sub(x, y):
+    z=x-y
+    print ('%d - %d = %d\n' %(x, y, z))
+
+
+def Mul(x, y):
+    z=x*y
+    print ('%d x %d = %d\n' %(x, y, z))
+
+
+def Div(x, y):
+    z=x/y
+    print ('%d / %d = %d\n' %(x, y, z))
+
+Add(3,4)
+Sub(3,4)
+Sub(4,3)
+Mul(3,4)
+Div(4,3)
+Div(3,4)
